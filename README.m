a4883d70d2022f17f344e048f364aaf123c2fa55
wandersongabi <144235877+wandersongabi@users.noreply.github.com>
2023-09-13 08:57:46 -0300

    Create README.m

---
+++ README.m
@@ -0,0 +1,6 @@
+ola meu nome e wanderson tenho 15 anos estou estudando no colegio estadual branca da mota
+estou estundando por meio do alura;me desenvolvendo na linguagem scratch e javascript;
+utilizo esse espaço para minha organização e compartilhamento dos meus projetos 
+- [Criando uma animação](https://classroom.google.com/c/NTg2ODQyNTM0OTAz/a/NTU0Mzk5Njk0MjMx/details)
+- [ meu projeto programado]( https://classroom.google.com/c/NTg2ODQyNTM0OTAz/a/NjEzMzAxMDQxMzA0/details)
+- [ animando uma palavran ]( https://classroom.google.com/c/NTg2ODQyNTM0OTAz/a/NTU0NDIzNTAxNzI0/details) 
